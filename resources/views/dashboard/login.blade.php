08114889555421e48df27e89c4c39c378d939dfa
noorfarihaf11 <noorfarihaf05@gmail.com>
2024-11-29 15:16:11 +0700

    fixing bugs

--- resources/views/dashboard/login.blade.php
+++ resources/views/dashboard/login.blade.php
@@ -7,6 +7,7 @@
     <title>Create account - Windmill Dashboard</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="../assets/css/tailwind.output.css" />
+    <link href="{{ asset('assets/css/tailwind.css') }}" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <script src="../assets/js/init-alpine.js"></script>
 </head>
